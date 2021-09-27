9b8cb87ede456c6025a9b44636b1a1de237039f9
Praveen-Bharath <praveen.b@tringapps.com>
2021-09-27 14:37:51 +0530

    second commit

---
+++ Crud Application/crudscript.js
@@ -0,0 +1,78 @@
+var selectedRow = null
+
+function onFormSubmit() {
+    if (validate()) {
+        var formData = readFormData();
+        if (selectedRow == null)
+            insertNewRecord(formData);
+        else
+            updateRecord(formData);
+        resetForm();
+    }
+}
+function readFormData() {
+    var formData = {};
+    formData["fullName"] = document.getElementById("fullName").value;
+    formData["age"] = document.getElementById("age").value;
+    formData["gender"] = document.getElementById("gender").value;
+    selectedElement = document.querySelector('.hobby1');
+    formData["hobby"] = selectedElement.value;
+    return formData;
+}
+function insertNewRecord(data) {
+    var table = document.getElementById("List").getElementsByTagName('tbody')[0];
+    var newRow = table.insertRow(table.length);
+    cell1 = newRow.insertCell(0);
+    cell1.innerHTML = data.fullName;
+    cell2 = newRow.insertCell(1);
+    cell2.innerHTML = data.age;
+    cell3 = newRow.insertCell(2);
+    cell3.innerHTML = data.gender;
+    cell4 = newRow.insertCell(3);
+    cell4.innerHTML = data.hobby;
+    cell4 = newRow.insertCell(4);
+    cell4.innerHTML = `<p onClick="onEdit(this)"><img src="edit.png" width=3%></p>
+                       <p onClick="onDelete(this)"><img src="delete.jfif" width=3%></p>`;
+}
+function resetForm() {
+    document.getElementById("fullName").value = "";
+    document.getElementById("age").value = "";
+    document.getElementById("gender").value = "";
+    document.getElementById("hobby").value = "";
+    selectedRow = null;
+}
+
+function onEdit(td) {
+    selectedRow = td.parentElement.parentElement;
+    document.getElementById("fullName").value = selectedRow.cells[0].innerHTML;
+    document.getElementById("age").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("gender").value = selectedRow.cells[2].innerHTML;
+    document.getElementById("hobby").value = selectedRow.cells[3].innerHTML;
+}
+function updateRecord(formData) {
+    selectedRow.cells[0].innerHTML = formData.fullName;
+    selectedRow.cells[1].innerHTML = formData.age;
+    selectedRow.cells[2].innerHTML = formData.gender;
+    selectedRow.cells[3].innerHTML = formData.hobby;
+}
+
+function onDelete(td) {
+    if (confirm('Are you sure to delete this record ?')) {
+        row = td.parentElement.parentElement;
+        document.getElementById("List").deleteRow(row.rowIndex);
+        resetForm();
+    }
+}
+function validate() {
+    isValid = true;
+    if (document.getElementById("fullName").value == "") {
+        isValid = false;
+        document.getElementById("fullNameValidationError").classList.remove("hide");
+    } else {
+        isValid = true;
+        if (!document.getElementById("fullNameValidationError").classList.contains("hide"))
+            document.getElementById("fullNameValidationError").classList.add("hide");
+    }
+    return isValid;
+}
+
