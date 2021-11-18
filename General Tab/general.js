19c5aad7a88d7460a54e6ad012f254ce7f6fb7c0
Praveen-Bharath <91447499+Praveen-Bharath@users.noreply.github.com>
2021-11-18 11:47:53 +0530

    Update general.js

--- General Tab/general.js
+++ General Tab/general.js
@@ -1,11 +1,1 @@
-function Breakfast(){
-    var x=document.createElement("button")
-}
-function Lunch(){
-    var x=document.createElement("button")
-}
-document.getElementById("top1").addEventListener("click", myFunction);
 
-function myFunction() {
-  document.getElementById("top1");
-}
